9d465dd6558262ca75dcaad7abd13d58f383aeaa
hariom kumar <hariomtech27@gmail.com>
2026-01-30 12:10:48 +0530

    city

---
+++ Delta.API/Controllers/Utilities/ICityController.cs
@@ -0,0 +1,37 @@
+﻿using Delta.Domain.Entities.Utilities;
+using Delta.Infrastructure.Persistence.EF;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Delta.API.Controllers.Utilities
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CityController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CityController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ICity>>> GetCities()
+        {
+            var cities = await _context.Cities.ToListAsync();
+            return Ok(cities);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ICity>> GetCity(int id)
+        {
+            var city = await _context.Cities.FindAsync(id);
+            if (city == null)
+                return NotFound();
+
+            return Ok(city);
+        }
+    }
+}
---
+++ Delta.Domain/Entities/Utilities/ICity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delta.Domain.Entities.Utilities
+{
+    public class ICity
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+    }
+}
