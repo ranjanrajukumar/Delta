934b87a532213958f5a1841a83f1791ee362a982
hariom kumar <hariomtech27@gmail.com>
2026-01-30 12:21:27 +0530

    city

--- Delta.Domain/Entities/Utilities/ICity.cs
+++ Delta.Domain/Entities/Utilities/ICity.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Delta.Domain.Entities.Utilities
 {
+    [Table("City")]
     public class ICity
     {
         public int CityId { get; set; }
